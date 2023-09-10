ba5ed8a7dc9cb3c8640559edc9ac2e05f0faf068
Christian Ezeanyaeche <christianezeanyaeche@gmail.com>
2023-09-10 14:11:54 +0100

    fix: add /api

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   const { slack_name, track } = req.query;
   if (!slack_name || !track) {
     return res.status(400).json({
